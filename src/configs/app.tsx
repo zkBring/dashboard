ee487bd1f18cd417c13ada0ebbb340edefd5116b
Hazo Baykulov <hazobaykulov@iMac-Hazo.local>
2022-06-30 23:36:36 +0300

    added fix for factory

--- src/configs/app.tsx
+++ src/configs/app.tsx
@@ -1,7 +1,7 @@
 export const NATIVE_TOKEN_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const LINK_COMISSION_PRICE = '0.00'
 export const EXPIRATION_DATE = '1900000000000'
-export const CLAIM_APP = 'https://v1-2.claim.linkdrop.io'
+export const CLAIM_APP = 'https://frolicking-phoenix-d90f14.netlify.app'
 export const CLAIM_APP_AURORA = 'https://aurora-claim.linkdrop.io'
 export const CLAIM_APP_QR = 'https://frolicking-phoenix-d90f14.netlify.app'
 export const IPFS_GATEWAY_URL = "https://gateway.pinata.cloud/ipfs/"
--- src/configs/contracts.tsx
+++ src/configs/contracts.tsx
@@ -6,22 +6,22 @@ type TContracts = {
 
 const contracts: TContracts = {
   4: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480'
+    factory: '0x33C5640DD2C448bB7c3Dcca8ade3268fED092403'
   }, // rinkeby
   1: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480'
+    factory: '0x33C5640DD2C448bB7c3Dcca8ade3268fED092403'
   }, // mainnet
   137: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480'
+    factory: '0x33C5640DD2C448bB7c3Dcca8ade3268fED092403'
   }, // polygon
   1313161554: {
     factory: '0xe1C9135600e399eE87B4392Fc56ecC9F85C33713'
   }, // aurora
   80001: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480'
+    factory: '0x33C5640DD2C448bB7c3Dcca8ade3268fED092403'
   }, // mumbai
   100: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480'
+    factory: '0x33C5640DD2C448bB7c3Dcca8ade3268fED092403'
   } // xdai
 }
 
