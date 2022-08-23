3c8f63787eb95a0df466c34b3baff852f1fd86d2
Hazo Baykulov <hazobaykulov@iMac-Hazo.local>
2022-08-23 15:12:51 +0300

    added fixes for contracts

--- src/configs/contracts.tsx
+++ src/configs/contracts.tsx
@@ -7,27 +7,27 @@ type TContracts = {
 
 const contracts: TContracts = {
   4: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   }, // rinkeby
   1: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   }, // mainnet
   137: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   }, // polygon
   1313161554: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   }, // aurora
   80001: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   }, // mumbai
   100: {
-    factory: '0x9A9E408171Bd8A497bbe89D149A8eb37B231bE7a',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     minter_role: '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6'
   } // xdai
 }
