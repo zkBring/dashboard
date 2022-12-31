51a467e822305b76e139f9fd78279c69f5a5957e
spacehaz <spacehaz@gmail.com>
2022-12-31 10:02:40 +0100

    minor fixes

--- src/components/common/mini-popup/styled-components.tsx
+++ src/components/common/mini-popup/styled-components.tsx
@@ -4,14 +4,15 @@ export const MiniPopupContainerClass = 'MiniPopupContainer'
 
 export const MiniPopupContainer = styled.div`
   &.${MiniPopupContainerClass} {
-    padding: 16px;
+    padding: 8px 16px;
     border: 1px solid ${props => props.theme.primaryBorderColor};
     border-radius: 20px;
-    background: ${props => props.theme.blankColor};
+    background: ${props => props.theme.backgroundColor};
     min-width: 152px;
     position: absolute;
     transform: translateY(calc(100% + 10px));
     bottom: 0;
     z-index: 2;
+    right: 0;
   }
 `
