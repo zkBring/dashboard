2b21c226f01ea5d908aeee88ce72b6114d69f53b
spacehaz <spacehaz@gmail.com>
2023-12-14 12:28:39 +0100

    added new configs

--- src/configs/address-specific-options.tsx
+++ src/configs/address-specific-options.tsx
@@ -18,32 +18,17 @@ const COINBASE_CONFIG = {
   qrDeeplink: 'https://go.cb-w.com/dapp?cb_url=%URL%'
 }
 
+const ALPHEMY_CONFIG = {
+  claimAppUrl: 'https://alphemy.linkdrop.io'
+}
+
 export const addressSpecificOptions: TAddressConfig = {
   '0xe78f82c63f9237ce12c415afc547fa80e1575629': COINBASE_CONFIG,
   '0x902b0bc91546a454bc70320dd9917f0def81c033': COINBASE_CONFIG,
   '0xd756da6e34522f534a811c96e5e3668b38a76a22': COINBASE_CONFIG,
   '0xf96f058d4197f0e8e8284ae4123b38e4701c93fd': COINBASE_CONFIG,
-  '0xf2b95635fc6cf3ebc8b5ca01c4e683a22b2e662c': {
-    ...COINBASE_CONFIG,
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0xb4c3d57327d4fc9bcc3499963e21db1a5435d537': COINBASE_CONFIG,
-  '0x3e12f31db4f1250e077171568263d0856d897428': {
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0x0553ada5829184a7818dc866367d77334183603e': {
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0x274e7610d931c7008373a70de780e68a010872c7': {
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0x1dadf1f992ed2448baa284d0fca6a62536136612': {
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0xe2f91e21c6689382c1c3dd8cd70633c39c911c4a': {
-    betaFeaturesAvailable: ['dispenser']
-  },
-  '0xb7d98f28d9f7ef2732802c62bcac12c1b0d5edfb': {
-    betaFeaturesAvailable: ['dispenser']
-  }
+  '0xf2b95635fc6cf3ebc8b5ca01c4e683a22b2e662c': COINBASE_CONFIG,
+  '0xf92f4c77ab90b6e0ee2ccb24fa176d151c5ba9d0': ALPHEMY_CONFIG,
+  '0x241365e6eed258ac4a2824424b6802c0240e13eb': ALPHEMY_CONFIG,
+  '0xb4c3d57327d4fc9bcc3499963e21db1a5435d537': COINBASE_CONFIG
 }
