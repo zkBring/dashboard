864e7c3261511cc34667fc6b96cba7f7bf4d5b61
spacehaz <spacehaz@gmail.com>
2024-03-25 14:03:14 +0300

    added config for claim url schema

--- src/helpers/decrypt-links.tsx
+++ src/helpers/decrypt-links.tsx
@@ -1,17 +1,20 @@
 import { TLink, TLinkDecrypted }  from 'types'
 import { decrypt } from 'lib/crypto'
-import { defineClaimAppURL } from 'helpers'
+import { defineClaimAppURL, defineUrlSchema } from 'helpers'
 
 type TDecryptLink = ({
   links,
   dashboardKey,
   tokenAddress,
-  userAddress
+  userAddress,
+  chainId
 } : {
   links: TLink[],
   dashboardKey: string,
   tokenAddress: string,
-  userAddress: string
+  userAddress: string,
+  chainId: number
+
 }
 ) => TLinkDecrypted[]
 
@@ -19,7 +22,8 @@ const decryptLinks: TDecryptLink = ({
   links,
   dashboardKey,
   tokenAddress,
-  userAddress
+  userAddress,
+  chainId
 }) => {
   const decryptedLinks = []
   const start = +new Date()
@@ -35,13 +39,20 @@ const decryptLinks: TDecryptLink = ({
       const encryptedClaimCode = links[i].encrypted_claim_code
       if (encryptedClaimCode) {
         const decryptedClaimCode = decrypt(encryptedClaimCode, dashboardKey)
+        const claimUrl = defineUrlSchema(
+          userAddress,
+          decryptedClaimCode,
+          chainId,
+          claimAppURL,
+          3 // version
+        )
         decryptedLinks.push({
           link_id: links[i].link_id,
           token_id: links[i].token_id !== '0' ? links[i].token_id : '0',
           token_amount: links[i].token_amount !== '0' ? links[i].token_amount : undefined,
           token_address: tokenAddress,
           claim_code: decryptedClaimCode,
-          claim_link: `${claimAppURL}/#/redeem/${decryptedClaimCode}?src=d`
+          claim_link: claimUrl
         })
       }
     }
