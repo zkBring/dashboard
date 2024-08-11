59aad8c4fd7649cd801834910cefba69bb92b9ab
spacehaz <spacehaz@gmail.com>
2024-08-11 21:31:37 +0300

    update claim link urls

--- src/helpers/define-url-schema.tsx
+++ src/helpers/define-url-schema.tsx
@@ -1,8 +1,46 @@
+const {
+  REACT_APP_CLIENT
+} = process.env
+import {
+  COINBASE_CLAIM_URL
+} from 'configs/app'
+
 const defineUrlSchema = (
   decryptedClaimCode: string,
-  claimAppURL: string
+  chainId: number,
+  claimAppURL: string,
+  version: number,
+  wallet: string,
+  availableWallets: string[]
 ) => {
-  const url = `${claimAppURL}/#/redeem/${decryptedClaimCode}?src=d`
+
+  if (REACT_APP_CLIENT === 'coinbase') {
+    const url = COINBASE_CLAIM_URL
+      .replace('<CODE>', decryptedClaimCode)
+      .replace('<CHAIN_ID>', String(chainId))
+      .replace('<VERSION>', String(version))
+
+    return url
+  }
+  const defaultLink = `${claimAppURL}/#/redeem/${decryptedClaimCode}?src=d`
+
+  if (wallet !== 'coinbase_wallet') {
+    return defaultLink
+  }
+
+  if (availableWallets[0] !== 'coinbase_wallet') {
+    return defaultLink
+  }
+
+  if (availableWallets.length !== 1) {
+    return defaultLink
+  }
+
+  const url = COINBASE_CLAIM_URL
+    .replace('<CODE>', decryptedClaimCode)
+    .replace('<CHAIN_ID>', String(chainId))
+    .replace('<VERSION>', String(version))
+
   return url
 }
 
